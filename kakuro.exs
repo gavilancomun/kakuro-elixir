d196fdda074d5e17604d74fe1fcd0d7b5f0d5f77
James Petry <gavilancomun@gmail.com>
2015-05-26 16:07:10 +0100

    init

--- kakuro.exs
+++ kakuro.exs
@@ -31,29 +31,50 @@ def draw_v(values, x) do
   end
 end
 
+def catstr(coll) do
+  Enum.reduce(coll, fn (v, acc) -> acc <> v end)
+end
+
 def draw({:empty}) do "   -----  " end
-def draw({:down, n}) do :io_lib.format("   ~2B\\--  ", [n]) end
-def draw({:across, n}) do :io_lib.format("   --\\~2B  ", [n]) end
-def draw({:down_across, d, a}) do :io_lib.format("   ~2B\\~2B  ", [d, a]) end
+def draw({:down, n}) do :io_lib.format("   ~2B\\--  ", [n]) |> List.flatten |> to_string end
+def draw({:across, n}) do :io_lib.format("   --\\~2B  ", [n]) |> List.flatten |> to_string end
+def draw({:down_across, d, a}) do :io_lib.format("   ~2B\\~2B  ", [d, a]) |> List.flatten |> to_string end
 def draw({:value, values}) do 
   case Set.size(values) == 1 do
-    true -> Enum.reduce((for x <- Set.to_list(values), do: "     " <> Integer.to_string(x) <> "    "), fn(v, acc) -> acc <> v end )
-    _ -> " " <> Enum.reduce((for x <- [1, 2, 3, 4, 5, 6, 7, 8, 9], do: draw_v(values, x)), fn (v, acc) -> acc <> v end)
+    true -> catstr(for x <- Set.to_list(values), do: "     " <> Integer.to_string(x) <> "    ")
+    _ -> " " <> catstr(for x <- [1, 2, 3, 4, 5, 6, 7, 8, 9], do: draw_v(values, x))
   end
 end
 
+def draw_row(row) do
+  catstr(for x <- row, do: draw(x)) <> "\n"
+end
+
+def draw_grid(grid) do
+  "\n" <> catstr(for x <- grid, do: draw_row(x))
+end
+
+def grid1() do 
+  [[e(), d(4), d(22), e(), d(16), d(3)],
+   [a(3), v(), v(), da(16, 6), v(), v()],
+   [a(18), v(), v(), v(), v(), v()],
+   [e(), da(17, 23), v(), v(), v(), d(14)],
+   [a(9), v(), v(), a(6), v(), v()],
+   [a(15), v(), v(), a(12), v(), v()]]
+end
+
 def main() do
-  IO.puts draw(e());
-  IO.puts draw(a(12));
-  IO.puts draw(d(12));
-  IO.puts draw(da(12, 6));
-  IO.puts draw(v());
-  IO.puts draw(v([1, 3, 7]));
+  IO.puts draw(e())
+  IO.puts draw(a(12))
+  IO.puts draw(d(12))
+  IO.puts draw(da(12, 6))
+  IO.puts draw(v())
+  IO.puts draw(v([1, 3, 7]))
+  IO.puts draw_grid(grid1())
   :ok
 end
 
 end
-
 
 Kakuro.main()
 
